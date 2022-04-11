20d304122b107b11db7f21243c380a221d5321fc
Hieu X. Leu <hieuleuxuan@gmail.com>
2022-04-12 09:31:55 +1000

    update code 11

--- 11. Container With Most Water/main.cpp
+++ 11. Container With Most Water/main.cpp
@@ -5,12 +5,12 @@ public:
         int max_area = 0;
         while(l<r){ //設定左指標,右指標
             
-            int t_max = min(height[l],height[r])*(r-l); //每次都先計算圍成的面積
+            int t_max = min(height[l],height[r])*(r-l); 
             
             if(t_max > max_area)
                 max_area = t_max;
             
-            if(height[l] <= height[r])//移動小的那一根就好 移動大的反而會讓面積更小
+            if(height[l] <= height[r])
                 l++;
             else
                 r--;
